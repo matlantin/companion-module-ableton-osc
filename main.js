fcb866fdec6f8f3067cfc94c4392039cf8851636
Mathieu Lantin <47821824+matlantin@users.noreply.github.com>
2025-12-03 20:05:24 +0100

    fix: change fade in curve to cubic ease-in for even better low-end resolution

--- main.js
+++ main.js
@@ -204,9 +204,9 @@ class AbletonOSCInstance extends InstanceBase {
 					const remaining = 1.0 - progress
 					newValue = fade.startValue * (remaining * remaining)
 				} else {
-					// Quadratic fade in (Ease-In: starts slow, speeds up)
-					// This gives more resolution in the low range (0-75%)
-					newValue = fade.startValue * (progress * progress)
+					// Cubic fade in (Ease-In: starts very slow, speeds up)
+					// This gives even more resolution in the low range (0-75%)
+					newValue = fade.startValue * (progress * progress * progress)
 				}
 				
 				if (fade.type === 'clip') {
